b295651e517c5647a2ed7674643e835f1d8d0c9e
Nicolas Skuček <nico.skucek@gmail.com>
2024-06-19 13:19:31 +0200

    Added FileController with simple endpoints.

---
+++ src/MSoC_API/Controllers/FileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSoC_API.Controllers;
+
+[ApiController]
+public class FileController
+{
+    [HttpGet("api/files")]
+    public Task GetAllFiles()
+    {
+        return Task.CompletedTask;
+    }
+
+    [HttpGet("api/files/{fileName}")]
+    public Task GetFile([FromRoute] string fileName)
+    {
+        return Task.CompletedTask;
+    }
+
+}
