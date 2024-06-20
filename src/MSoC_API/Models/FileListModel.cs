40d4d75cf1b667b5452c1abaa8a70c54f866dd90
Nicolas Skuček <nico.skucek@gmail.com>
2024-06-20 08:38:46 +0200

    Added method to retrieve all files from specified directory.

---
+++ src/MSoC_API/Models/FileListModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MSoC_API.Models;
+
+public class FileListModel
+{
+    public string Directory { get; set; } = null!;
+    public string[] FileNames { get; set; } = null!;
+}
---
+++ src/MSoC_API/Services/FileService.cs
@@ -0,0 +1,25 @@
+﻿using MSoC_API.Models;
+using Newtonsoft.Json;
+
+namespace MSoC_API.Services;
+
+public class FileService
+{
+    public string GetAllFilesInFolder()
+    {
+        var d = new DirectoryInfo(@"./FileSystem");//test folder with random .txt files
+
+        var files = d.GetFiles();//get all files in folder
+
+        
+        var fileList = new FileListModel
+        {
+            Directory = d.FullName, // full path to folder
+            FileNames = files.Select(f => f.Name).ToArray(), // select only names of folders, and convert to array
+        };
+
+        var json = JsonConvert.SerializeObject(fileList); //serialize FileListModel to json
+
+        return json;
+    }
+}
